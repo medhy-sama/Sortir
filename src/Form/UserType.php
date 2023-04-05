63edd08b6e43954fef61bea26ffd4d018aae941c
mboulon2022 <medhy.boulon2022@campus-eni.fr>
2023-04-05 14:49:30 +0200

    :construction: css en phase de décoration avancé

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -27,7 +27,9 @@ class UserType extends AbstractType
     {
 
         $builder
-            ->add('username')
+            ->add('username', TextType::class, [
+                'label' => 'Username : '
+            ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 // instead of being set onto the object directly,
@@ -48,24 +50,24 @@ class UserType extends AbstractType
                 'invalid_message' => 'Attention les valeurs des champs de mot de passe ne correspondent pas.',
                 'options' => ['attr' => ['class' => 'password-field']],
                 'required' => true,
-                'first_options'  => ['label' => 'Mot de passe'],
-                'second_options' => ['label' => 'Confirmer mot de passe'],
+                'first_options'  => ['label' => 'Mot de passe : '],
+                'second_options' => ['label' => 'Confirmer mot de passe : '],
 
             ])
             ->add('nom', TextType::class, [
-                'label' => 'Nom  : '
+                'label' => 'Nom : '
                 ])
             ->add('prenom', TextType::class, [
-                'label' => 'Prénom  : '
+                'label' => 'Prénom : '
             ])
-            ->add('telephone', IntegerType::class, [
-                'label' => 'Téléphone  : '
+            ->add('telephone', TextType::class, [
+                'label' => 'Téléphone : '
             ])
             ->add('mail', TextType::class, [
-                'label' => 'Nom  : '
+                'label' => 'Email : '
             ])
             ->add('photo', FileType::class, [
-                'label' => 'Photo de Profil     : ',
+                'label' => 'Photo de Profil : ',
                 'mapped' => false,
                 'required' => false,
                 'constraints' => [
